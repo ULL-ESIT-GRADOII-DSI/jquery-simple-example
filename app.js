b0aac1d4fafe366daa765b6e15aded98c625df21
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-03-29 07:48:54 +0100

    jquery example

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var main = function () {
     var addCommentFromInputBox = function () {
         var $new_comment;
 
+        /* check to see if the content of the input box is empty */
         if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment = $("<p>"). // create a new paragraph element as a jQuery object, 
+              text(                  // change the text content
+                $(".comment-input input").val() // access the content of the input box
+              );
+            /* instead of the new comment just appearing */
             $new_comment.hide();
             $(".comments").append($new_comment);
+            /* let’s have it fade in... */
             $new_comment.fadeIn();
+            /* clear out the input box */
             $(".comment-input input").val("");
         }
     };
